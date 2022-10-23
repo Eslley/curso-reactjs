6dc12de26a38ab3ef806a9d0c14d2c50b77768b3
Eslley <rubemeslley@gmail.com>
2022-10-23 20:01:43 -0300

    Adicionando área de serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -13,7 +13,8 @@ function Project() {
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjetcForm] = useState(false)
     const [message, setMessage] = useState()
-    const[type, setType] = useState()
+    const [type, setType] = useState()
+    const [showServiceForm, setShowServiceForm] = useState(false)
 
     useEffect(() => {
         setTimeout(() => {
@@ -34,7 +35,7 @@ function Project() {
     function editPost(project) {
         setMessage('')
 
-        if(project.budget < project.cost) {
+        if (project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
             setType('error')
             return false
@@ -59,6 +60,10 @@ function Project() {
 
     function toggleProjectForm() {
         setShowProjetcForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
     }
 
     return (
@@ -90,6 +95,21 @@ function Project() {
                                 </div>
                             )}
                         </div>
+                        <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                                {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                            </button>
+                            <div className={styles.project_info}>
+                                {showServiceForm &&
+                                    <div>Formulario</div>
+                                }
+                            </div>
+                        </div>
+                        <h2>Serviços</h2>
+                        <Container customClass="start">
+                            <p>Itens de serviços</p>
+                        </Container>
                     </Container>
                 </div>
             )
