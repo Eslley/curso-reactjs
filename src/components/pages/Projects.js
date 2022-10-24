7a1992183a48848db7332b9f8996988e7454e66f
Eslley <rubemeslley@gmail.com>
2022-10-23 22:17:05 -0300

    Finalizando projeto

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -73,7 +73,7 @@ function Projects() {
                             handleRemove={removeProject} />
                     ))}
                 {!removeLoading && <Loading />}
-                {removeLoading && projects.length == 0 &&
+                {removeLoading && projects.length === 0 &&
                     <p>Não há projetos cadastrados!</p>
                 }
             </Container>
